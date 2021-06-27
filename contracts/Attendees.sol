f9148dc34ce963cebee01bf10f0e217729e82f09
JimLynchCodes <mrdotjim@gmail.com>
2021-06-27 19:09:28 -0400

    add initial PaymentSplitter code

--- contracts/Attendees.sol
+++ contracts/Attendees.sol
@@ -2,6 +2,8 @@
 pragma solidity >=0.4.22 <0.9.0;
 pragma experimental ABIEncoderV2;
 
+import "@openzeppelin/contracts/finance/PaymentSplitter.sol";
+
 contract Attendees {
     
     enum TShirtSize { SMALL, MEDIUM, LARGE, X_LARGE, XX_LARGE }
@@ -15,7 +17,11 @@ contract Attendees {
 
     address[] private attendees;
 
+    uint attendeesCount;
+
     mapping(address => AttendeeInfo) attendeeToInfo;
+
+    PaymentSplitter paymentSplitter;
 
     modifier restricted() {
         require(
@@ -26,19 +32,37 @@ contract Attendees {
     }
 
     function declareAttendance(string memory displayName, uint shirtSize) public {
+      
+      // TODO - require that the caller address was not banned.
 
       attendees.push(msg.sender);
-
       attendeeToInfo[msg.sender] = AttendeeInfo(displayName, shirtSize);
+
+      attendeesCount++;
     }
 
     function getMyInfo() public view returns (AttendeeInfo memory) {
-
       return attendeeToInfo[msg.sender];
-
     }
 
     function getAllAttendees() public view returns (address[] memory) {
       return attendees;
     }
+
+    function initiateAirdrop() public {
+
+      uint equalShares = [];
+
+      for (i; i < attendeesCount; i++) {
+        equalShares.push(1);
+      }
+
+      paymentSplitter = PaymentSplitter(attendees, equalShares);
+
+    }
+
+    function pullPayment() public {
+      paymentSplitter.release(msg.sender);
+    }
+
 }
